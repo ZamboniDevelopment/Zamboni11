a8f1c434407f3e1cef8e9992df436ceae741f586
kaapo <kaapo.pikkarainen@gmail.com>
2026-02-02 23:58:25 +0200

    This commit fixes a need for 2 game patcheses needed in nhl 11. Might have pulled half of my hair for figuring this out.

--- Components/NHL11/OSDKSettingsComponent.cs
+++ Components/NHL11/OSDKSettingsComponent.cs
@@ -13,37 +13,11 @@ internal class OSDKSettingsComponent : OSDKSettingsComponentBase.Server
     {
         return Task.FromResult(new FetchSettingsResponse
         {
-            mIntegerSettingList = new List<SettingInteger>
-            {
-                new SettingInteger
-                {
-                    mDefault = 0,
-                    mHelpLabel = "1",
-                    mId = "1",
-                    mLabel = "1",
-                    mLocalizedFields = 0,
-                    mPossibleValueMap = new SortedDictionary<uint, string>()
-                    {
-                        {
-                            0, "zero"
-                        },
-                        {
-                            1, "one"
-                        }
-                    },
-                    mToggles = 1
-                }
-            },
             mStringSettingList = new List<SettingString>
             {
                 new SettingString
                 {
-                    mDefault = "2",
-                    mHelpLabel = "2",
-                    mId = "2",
-                    mLabel = "2",
-                    mLocalizedFields = 0,
-                    mToggles = 1
+                    mId = "O_TKfilter",
                 }
             }
         });
@@ -57,43 +31,10 @@ internal class OSDKSettingsComponent : OSDKSettingsComponentBase.Server
             {
                 new SettingGroup
                 {
-                    mID = "1",
-                    mLSET = new List<string>
+                    mId = "O_SG_TCKR",
+                    mSettingList = new List<string>
                     {
-                        "1", "2"
-                    },
-                    mViewList = new List<SettingView>
-                    {
-                        new SettingView
-                        {
-                            mID = "1",
-                            mSettingViewDataList = new List<SettingViewData>
-                            {
-                                new SettingViewData
-                                {
-                                    mDefaultStr = "1",
-                                    mHelpLabel = "1",
-                                    mId = "1",
-                                    mToggles = 1,
-                                    mValueStr = "1"
-                                }
-                            }
-                        },
-                        new SettingView
-                        {
-                            mID = "2",
-                            mSettingViewDataList = new List<SettingViewData>
-                            {
-                                new SettingViewData
-                                {
-                                    mDefaultStr = "2",
-                                    mHelpLabel = "2",
-                                    mId = "2",
-                                    mToggles = 1,
-                                    mValueStr = "2"
-                                }
-                            }
-                        }
+                        "O_TKfilter"
                     }
                 }
             }
